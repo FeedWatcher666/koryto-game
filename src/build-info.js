98842856b9d0c2b29f18acb6ed82f2894660f1ea
FeedWatcher666 <skrivan.michael@gmail.com>
2026-07-27 22:12:50 +0200

    v0.16.9 TEST.2: reconcile legacy version writers centrally

--- src/build-info.js
+++ src/build-info.js
@@ -26,6 +26,34 @@
     return true;
   }
 
-  globalThis.KorytoBuildInfo = Object.freeze({...info, applyLabels});
+  function installLegacyReconciliation() {
+    if (typeof globalThis.setInterval === "function" && !globalThis.setInterval.__korytoBuildInfoWrapped) {
+      const nativeSetInterval = globalThis.setInterval.bind(globalThis);
+      const wrappedSetInterval = function(callback, delay, ...args) {
+        if (typeof callback !== "function") return nativeSetInterval(callback, delay, ...args);
+        const source = Function.prototype.toString.call(callback);
+        const writesLegacyVersion = /updateVersion|canonicalLabels/.test(source);
+        if (!writesLegacyVersion) return nativeSetInterval(callback, delay, ...args);
+        return nativeSetInterval(function(...callbackArgs) {
+          const result = callback(...callbackArgs);
+          queueMicrotask(applyLabels);
+          return result;
+        }, delay, ...args);
+      };
+      wrappedSetInterval.__korytoBuildInfoWrapped = true;
+      globalThis.setInterval = wrappedSetInterval;
+    }
+
+    if (typeof document !== "undefined" && !document.documentElement?.dataset?.korytoBuildReconcile) {
+      const reconcile = () => setTimeout(applyLabels, 140);
+      document.addEventListener("click", reconcile, true);
+      document.addEventListener("change", reconcile, true);
+      globalThis.addEventListener?.("load", applyLabels, {once: true});
+      if (document.documentElement?.dataset) document.documentElement.dataset.korytoBuildReconcile = "1";
+    }
+  }
+
+  globalThis.KorytoBuildInfo = Object.freeze({...info, applyLabels, installLegacyReconciliation});
+  installLegacyReconciliation();
   applyLabels();
 })();
