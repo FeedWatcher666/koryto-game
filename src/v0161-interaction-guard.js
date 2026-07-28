c3fbcb4f0a58c868269d8dc775ecfdb508ebb504
FeedWatcher666 <skrivan.michael@gmail.com>
2026-07-28 07:06:46 +0200

    v0.17.1 decision HUD: src/v0161-interaction-guard.js

--- src/v0161-interaction-guard.js
+++ src/v0161-interaction-guard.js
@@ -4,8 +4,8 @@
 
   function buildInfo() {
     return globalThis.KorytoBuildInfo || {
-      displayVersion: "0.17.0 TEST.1",
-      buildVersion: "0.17.0-test.1",
+      displayVersion: "0.17.1 TEST.1",
+      buildVersion: "0.17.1-test.1",
       saveVersion: "0.14.3-test.2",
       saveSchema: 1,
       applyLabels: () => false
