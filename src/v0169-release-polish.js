f5cf1d51ce9424b120c2cd7a1c730e9f0d8883a7
FeedWatcher666 <skrivan.michael@gmail.com>
2026-07-28 07:06:48 +0200

    v0.17.1 decision HUD: src/v0169-release-polish.js

--- src/v0169-release-polish.js
+++ src/v0169-release-polish.js
@@ -1,8 +1,8 @@
 "use strict";
 (() => {
   const INFO = globalThis.KorytoBuildInfo || Object.freeze({
-    displayVersion: "0.17.0 TEST.1",
-    buildVersion: "0.17.0-test.1",
+    displayVersion: "0.17.1 TEST.1",
+    buildVersion: "0.17.1-test.1",
     saveVersion: "0.14.3-test.2",
     saveSchema: 1,
     applyLabels: () => false
